66c4f05e92deac6d80ca832b72fc6fd862063fbd
Adrien Gregorj <adrien.gregorj@gmail.com>
2022-05-30 17:18:30 +0900

    remove unused function

--- code/preprocessing/utils.py
+++ code/preprocessing/utils.py
@@ -32,32 +32,3 @@ def pickle_save(file_path, data):
     """
     with open(file_path, "wb") as f:
         pk.dump(data, f)
-
-
-def parse_group_data(group_data):
-
-    ped_id = group_data[0]
-    group_size = group_data[1]
-
-    track_type = "normal"
-    if ped_id == -1:  # not tracked pedestrian
-        track_type = "missing"
-
-    elif ped_id < 0:  # non pedestrian
-        if group_size == -2:
-            track_type = "wheelchair"
-        elif group_size == -3:
-            track_type = "automatic_wheelchair"
-        else:
-            # print(group_data)
-            track_type = "baby_carriage"
-
-    group_members_ids = []
-    interacting_ids = []
-    if group_size > 0:
-        group_members_ids = group_data[2 : 2 + group_size - 1]
-        n_interacting = group_data[2 + group_size - 1]
-
-        interacting_ids = group_data[2 + group_size : 2 + group_size + n_interacting]
-
-    return track_type, ped_id, group_members_ids, interacting_ids
