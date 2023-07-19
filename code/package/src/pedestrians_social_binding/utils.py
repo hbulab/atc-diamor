dbbc24ba396a9022ee05c8e862c5b0135cf80693
Zewil08 <will.moiroud@hotmail.fr>
2023-07-19 10:51:00 +0900

    Here is the effect size begining

--- code/package/src/pedestrians_social_binding/utils.py
+++ code/package/src/pedestrians_social_binding/utils.py
@@ -1,4 +1,7 @@
 import pickle as pk
+import pandas as pd
+from scipy import stats
+import numpy as np
 
 
 def pickle_load(file_path: str):
@@ -31,3 +34,31 @@ def pickle_save(file_path: str, data):
     """
     with open(file_path, "wb") as f:
         pk.dump(data, f)
+
+
+def write_table(data : list, label : list):
+    cohen_data = np.zeros((len(data), len(data)))
+    t_statistic = np.zeros((len(data), len(data)))
+    for i, line in enumerate(data):
+        for j in range(i+1, len(data)):
+            t_statistic[i,j] = stats.ttest_ind(line, data[j])[1]
+            t_statistic[j,i] = None
+            cohen_data[i,j] = compute_cohen_d(line, data[j])
+            cohen_data[j,i] = None
+
+    final_data_stat = {"Labels / Data ": label,
+                  }
+    final_data_cohen = {"Labels / Data ": label,
+                  }
+    
+    for i,label_name in enumerate(label) :
+        final_data_stat[label_name] = t_statistic[i,:]
+        final_data_cohen[label_name] = cohen_data[i,:]
+
+    return [pd.DataFrame(final_data_stat),pd.DataFrame(final_data_cohen)]
+
+        
+def compute_cohen_d(group1 : list, group2 : list) :
+    n1 = len(group1)
+    n2 = len(group2)
+    return (np.mean(group1) - np.mean(group2)) / np.sqrt(((n1-1)*np.var(group1) + (n2-1)*np.var(group2)) / (n1 + n2 - 2))
